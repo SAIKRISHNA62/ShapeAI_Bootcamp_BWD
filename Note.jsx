297b3a44946749db2ff3ecf3064691b5e188912b
SAIKRISHNA62 <70429999+SAIKRISHNA62@users.noreply.github.com>
2021-06-26 22:53:35 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> details : </h1>
+      <p>
+        Name: SAIKRISHNA 
+        NUMBER: 9441542950
+        MAILID:19N85A0515SAIKRISHNA@GMAIL.COM
+      </p>
+      <div className="note">
+      <p>
+        Name: Raghu 
+        NUMBER: 9989956828
+        MAILID:19N85A0501Raghu@GMAIL.COM
+      </p>
+      </div>
+
+      <div className="note">
+      <p>
+        Name: Deepak
+        NUMBER: 6300034263
+        MAILID:19N85A0514Deepak@GMAIL.COM
+      </p>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default Note;
